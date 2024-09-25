69c39981ae552e307478a377a6eefb72ff312aa0
fatima-ezzahraalouane <falouane38@gmail.com>
2024-09-25 16:31:35 +0100

    boucles

--- EX12.c
+++ EX12.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
 int main(){
-    
+    int nombre, inverseNombre;
+
+    printf("Entrez un nombre entier de quatre chiffres:");
+    scanf("%d", &nombre);
+
+    inverseNombre = 
 }
