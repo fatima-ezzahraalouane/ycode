b17fafa051d59732133750aaa10307d565f908b4
fatima-ezzahraalouane <falouane38@gmail.com>
2024-09-26 11:27:31 +0100

    modifier le code

--- Variables/EX12.c
+++ Variables/EX12.c
@@ -3,8 +3,14 @@
 int main(){
     int nombre, inverseNombre;
 
-    printf("Entrez un nombre entier de quatre chiffres:");
+    printf("Entrez un nombre entier de 4 chiffres : ");
     scanf("%d", &nombre);
 
-    inverseNombre = 
+    inverseNombre = (nombre % 10) * 1000 +
+                    (nombre / 10 % 10) * 100 +
+                    (nombre / 100 %10) * 10 +
+                    (nombre / 1000);
+    printf("L'inverse de %d est : %d ", nombre, inverseNombre);
+
+    return 0;
 }
